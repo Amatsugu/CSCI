27123cee35f5b97e8d0a79b27a94631fdea08e11
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2015-11-10 22:05:01 -0500

    Test 2 Part 2

---
+++ src/com/LuminousVector/Excersies/ContactStore/InvalidContactException.java
@@ -0,0 +1,9 @@
+package com.LuminousVector.Excersies.ContactStore;
+
+public class InvalidContactException extends Exception
+{
+	public InvalidContactException(String message)
+	{
+		super(message);
+	}
+}
