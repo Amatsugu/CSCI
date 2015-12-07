c7ee055777b7e0e74c60f27107e493cb48bf45da
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2015-12-07 14:39:59 -0500

    Doctor Subclass

--- src/com/LuminousVector/Excersies/Inheritance/Person.java
+++ src/com/LuminousVector/Excersies/Inheritance/Person.java
@@ -3,9 +3,23 @@ package com.LuminousVector.Excersies.Inheritance;
 public class Person
 {
 	public String name;
-	
+	public int age;
+	public String gender;
+
 	public Person(String name)
 	{
+		this(name, 0, null);
+	}
+
+	public Person(String name, int age)
+	{
+		this(name, age, null);
+	}
+
+	public Person(String name, int age, String gender)
+	{
 		this.name = name;
+		this.age = age;
+		this.gender = gender;
 	}
 }
---
+++ src/com/LuminousVector/Projects/DoctorClass/Doctor.java
@@ -0,0 +1,29 @@
+package com.LuminousVector.Projects.DoctorClass;
+
+import com.LuminousVector.Excersies.Inheritance.Person;
+
+public class Doctor extends Person
+{
+	public String specialty;
+
+	public Doctor(String name)
+	{
+		this(name, 0, null, null);
+	}
+
+	public Doctor(String name, String specialty)
+	{
+		this(name, 0, null, specialty);
+	}
+
+	public Doctor(String name, int age, String specialty)
+	{
+		this(name, age, null, specialty);
+	}
+
+	public Doctor(String name, int age, String gender, String specialty)
+	{
+		super(name, age, gender);
+		this.specialty = specialty;
+	}
+}
